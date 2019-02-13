e20c795ba9e32810cfcc7bef94e83ada37698ff9
yachitha <yachithasandaruwan@gmail.com>
2019-02-13 23:26:15 +0530

    changes to customer and loan controllers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Customer;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
@@ -12,7 +13,7 @@ class CustomerController extends Controller
     public function createCustomer(Request $request){
         $validate = Validator::make ($request->all (),[
             'name' => 'required|string|max:255',
-            'customer_no'=>'required|unique',
+            'customer_no'=>'required|unique:customer',
             'nic'=>'required|string|min:10',
             'contact_no'=>'required|regex:/[0-9]{9}/',
             'addLine1'=>'required',
@@ -28,14 +29,14 @@ class CustomerController extends Controller
                 $customer = Customer::create([
                     'customer_no'=>$request->customer_no,
                     'name' => $request->name,
-                    'email' => $request->name."001"."@gmail.com",
                     'NIC'=>$request->nic,
                     'addLine1'=>$request->addLine1,
                     'addLine2'=>$request->addLine2,
                     'city'=>$request->city,
                     'contact_no'=>$request->contact_no,
                     'status'=>"active",
-                    'salesRep_id'=>$request->salesRep_id
+                    'salesRep_id'=>$request->salesRep_id,
+                    'route_id'=>$request->route_id
                 ]);
                 if ($customer){
                     return redirect ('/createCustomer')->with ('message','Customer created successfully');
@@ -46,5 +47,18 @@ class CustomerController extends Controller
 
         }
 
+    }
+
+    public function getCustomers() {
+        try{
+            $customers = DB::table ('customer')->get();
+            if(!$customers->isEmpty ()){
+                return view ('Test.customerNumbers',['error'=>false, 'customers'=> $customers]);
+            } else {
+                return view ('Test.customerNumbers',['error'=>true, 'message'=>"Customer List Empty"]);
+            }
+        }catch (\Exception $e) {
+            return view ('Test.customerNumbers',['error'=>true, 'message'=>"Unknown error occurred"]);
+        }
     }
 }
--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Cash_book;
 use App\Customer;
 use App\Customer_loan;
 use App\Customer_repayment;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
@@ -27,7 +30,8 @@ class LoanController extends Controller
             return redirect ('/createLoan')->withErrors ($validate)->withInput ();
         }else{
             try{
-                if(Customer::where('customer_no',$request->customer_no)->first()){
+                $customer = Customer::where('customer_no',$request->customer_no)->first();
+                if($customer){
                     $loan = Customer_loan::create([
                         'loan_no'=>$request->loan_no,
                         'interest_rate' => $request->interest_rate,
@@ -36,16 +40,32 @@ class LoanController extends Controller
                         'no_of_installments'=>$request->no_of_installments,
                         'start_date'=>$request->start_date,
                         'end_date'=>$request->end_date,
-                        'customer_id'=>Customer::where('customer_no',$request->customer_no)->first()->id,
+                        'customer_id'=>$customer->id,
                         'duration'=>$request->no_of_installments,
                     ]);
 
                     if ($loan) {
+
+                        $cash_withdraw = new Cash_book();
+                        $cash_withdraw->transaction_date = Carbon::now ()->format ("Y-m-d H:i:s");
+                        $cash_withdraw->description = "given new loan to ".$customer->name;
+                        $cash_withdraw->deposit = 0.0;
+                        $cash_withdraw->withdraw = $loan->loan_amount;
+                        $last_balance_row = DB::table ( 'cash_book' )->orderBy ( 'id' , 'desc' )->first ();
+                        if ($last_balance_row){
+                            $cash_withdraw->balance = $last_balance_row->balance - $loan->loan_amount;
+                        }else {
+                            $cash_withdraw->balance = -$loan->amount;
+                        }
+
+                        $cash_withdraw->save ();
+
+
                         $repayment = new Customer_repayment();
                         $repayment->loan_id = $loan->id;
                         $repayment->bank_book_id = null;
                         $repayment->cash_book_id = null;
-                        $repayment->amount = $request->loan_amount;
+                        $repayment->amount = 0.0;
                         $repayment->installment_count = $request->no_of_installments;
                         $repayment->remaining_amount = $request->loan_amount;
                         $repayment->save ();
