eb4e33d4f66d9a260544495065b2eb2765ff3292
yachitha <yachithasandaruwan@gmail.com>
2019-02-13 23:22:29 +0530

    added Expenses Controllers

---
+++ app/Http/Controllers/Expenses/ExpenseController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yachitha Sandaruwan
+ * Date: 12/9/2018
+ * Time: 11:23 PM
+ */
+
+namespace App\Http\Controllers\Expenses;
+
+
+use App\Http\Controllers\Controller;
+
+class ExpenseController extends Controller
+{
+    function filterRecords() {}
+
+}
