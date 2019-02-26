c90295c09a18846a3d4b8839471ca7b1610736d1
yachitha <yachithasandaruwan@gmail.com>
2019-02-26 06:55:21 +0530

    refactor alignment

--- app/Bank_book.php
+++ app/Bank_book.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Bank_book extends Model
 {
     protected $table = "bank_book";
-    protected $fillable =[
-        'transaction_date','description', 'deposit', 'withdraw', 'balance', 'cheque_no'
+    protected $fillable = [
+        'transaction_date', 'description', 'deposit', 'withdraw', 'balance', 'cheque_no'
     ];
 
-    public function hasRepayments(){
-    return $this->hasMany ('App\Customer_repayment', 'bank_book_id');
-}
+    public function hasRepayments()
+    {
+        return $this->hasMany('App\Customer_repayment', 'bank_book_id');
+    }
 }
