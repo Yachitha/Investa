cb1c3491ddea8d5efbb222af22ce31bbcbd34552
yachitha <yachithasandaruwan@gmail.com>
2019-03-05 01:51:52 +0530

    new migration added

---
+++ database/migrations/2019_03_02_130723_create_sales_rep_salary_payment_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesRepSalaryPaymentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_rep_salary_payment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('payment_date');
+            $table->double('payment_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_rep_salary_payment');
+    }
+}
