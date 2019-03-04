b7e48bd41e25c4ac304ac01260652e18e2f56011
yachitha <yachithasandaruwan@gmail.com>
2019-03-05 01:48:50 +0530

    added new properties

--- app/Bank_book.php
+++ app/Bank_book.php
@@ -4,6 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string transaction_date
+ * @property string description
+ * @property float deposit
+ * @property mixed withdraw
+ * @property mixed cheque_no
+ * @property int balance
+ * @property mixed id
+ */
 class Bank_book extends Model
 {
     protected $table = "bank_book";
