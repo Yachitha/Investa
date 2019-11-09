583ec59b4c576b7a5d71012cf9c768470e4e152e
Yachitha Sandaruwan <yachithasandaruwan@gmail.com>
2019-11-09 11:08:21 +0530

    changes to Locker model

--- app/Locker.php
+++ app/Locker.php
@@ -5,6 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property mixed date
+ * @property mixed amount
+ * @property mixed cash_out
+ * @property null extra_str1
+ * @property int extra_int1
+ * @property float extra_float1
+ */
 class Locker extends Model
 {
     use SoftDeletes;
