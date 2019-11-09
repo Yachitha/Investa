1bd88c769cabc1b353767ce0cfe4af302e094ecb
Yachitha Sandaruwan <yachithasandaruwan@gmail.com>
2019-11-09 10:52:13 +0530

    add columns to customer table

---
+++ database/migrations/2019_10_09_121212_add_column_to_customer_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnToCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customer', function (Blueprint $table) {
+            $table->string('str_code')->default("A");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customer', function (Blueprint $table) {
+            //
+        });
+    }
+}
