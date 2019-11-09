1c0e281f983ad95d28bf8762e408bad3bad47706
Yachitha Sandaruwan <yachithasandaruwan@gmail.com>
2019-11-09 10:47:55 +0530

    changes to supplier loan table

--- database/migrations/2018_07_09_140259_create_supplier_loan_table.php
+++ database/migrations/2018_07_09_140259_create_supplier_loan_table.php
@@ -15,13 +15,23 @@ class CreateSupplierLoanTable extends Migration
     {
         Schema::create('supplier_loan', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer ('loan_no');
-            $table->float ('loan_amount');
-            $table->float ('interest_rate');
-            $table->float ('installment_amount');
-            $table->integer ('no_of_installments');
-            $table->date ('start_date');
-            $table->date ('end_date');
+            $table->integer ('supplier_id');
+            $table->date('date')->nullable();
+            $table->float ('amount');
+            $table->float ('interest_rate')->nullable();
+            $table->float ('installment_amount')->nullable();
+            $table->integer ('no_of_installments')->nullable();
+            $table->date ('start_date')->nullable();
+            $table->date ('end_date')->nullable();
+            $table->date('finished_date')->nullable();
+            $table->boolean('isFinished')->nullable();
+            $table->float('due_amount')->nullable();
+            $table->string('type');
+            $table->string('bank')->nullable();
+            $table->date('banking_date')->nullable();
+            $table->integer('cheque_no')->nullable();
+            $table->integer('cash_book_id')->nullable();
+            $table->integer('bank_book_id')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
